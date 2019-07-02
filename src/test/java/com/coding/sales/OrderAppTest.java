f3d54b8e2bdd03320e1ba7e311ef39d7f7bddf74
1003381120@qq.com <1003381120@qq.com>
2019-07-02 22:54:30 +0800

    新增测试案列2

--- src/test/java/com/coding/sales/OrderAppTest.java
+++ src/test/java/com/coding/sales/OrderAppTest.java
@@ -27,7 +27,8 @@ public class OrderAppTest {
     public static Collection<Object[]> parameters() {
         Object[][] data = new Object[][]{
         	{"sample_command.json", "sample_result.txt"},
-                {"One_command.json", "TestOne_result.txt"}
+            {"One_command.json", "TestOne_result.txt"},
+            {"two_Input.json", "two_Output.txt"}
                 
         };
 
